43c3f421b2f256434e4cc73d69e81d3ccd8ede01
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-09-02 18:06:42 +0200

    Use a symbol for the plugin declaration

--- lib/smart_proxy_ipam/ipam.rb
+++ lib/smart_proxy_ipam/ipam.rb
@@ -1,6 +1,6 @@
 module Proxy::Ipam
   class Plugin < ::Proxy::Plugin
-    plugin 'externalipam', Proxy::Ipam::VERSION
+    plugin :externalipam, ::Proxy::Ipam::VERSION
 
     http_rackup_path File.expand_path('ipam_http_config.ru', __dir__)
     https_rackup_path File.expand_path('ipam_http_config.ru', __dir__)
